1c53cd90437a35dd693b139f8a42be77b198533c
Gabriel Nicolás González Ferreira <gabrielinuz@gmail.com>
2016-10-03 10:29:43 -0300

    tabs by spaces

--- autoloader/ConcretePathsProvider.php
+++ autoloader/ConcretePathsProvider.php
@@ -1,7 +1,6 @@
 <?php
 /**
 * Copyright (c) Gabriel Ferreira <gabrielinuz@gmail.com>. All rights reserved. 
-* This file is part of classset-php.
 * Released under the GPL3 license
 * https://opensource.org/licenses/GPL-3.0
 **/
